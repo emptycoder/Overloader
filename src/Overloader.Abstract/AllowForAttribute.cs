c4fdce20e5731e902b9ac2cc37d7f9f79f778223
emptycoder <yaroslav200124@gmail.com>
2023-05-20 12:29:37 +0200

    Add reason field for IgnoreFor, AllowFor attributes.

--- src/Overloader.Abstract/AllowForAttribute.cs
+++ src/Overloader.Abstract/AllowForAttribute.cs
@@ -3,5 +3,5 @@ namespace Overloader;
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
 public class AllowForAttribute : Attribute
 {
-	public AllowForAttribute(Type? type = null) { }
+	public AllowForAttribute(Type? type = null, string? reason = null) { }
 }
--- src/Overloader.Abstract/IgnoreForAttribute.cs
+++ src/Overloader.Abstract/IgnoreForAttribute.cs
@@ -3,5 +3,5 @@ namespace Overloader;
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
 public class IgnoreForAttribute : Attribute
 {
-	public IgnoreForAttribute(Type? type = null) { }
+	public IgnoreForAttribute(Type? type = null, string? reason = null) { }
 }
